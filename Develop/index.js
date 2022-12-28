f349902dd6d79a5fdc605eb5f3e0d9f8f37aa2cf
Alex Robertson <alex.robertson390@gmail.com>
2022-12-28 15:37:22 -0800

    working file updated

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,94 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "projectTitle",
+            message:"What is the title of your project?"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Please type a description of your project"
+        },
+        {
+            type: "input",
+            name: "installationInstructions",
+            message: "Please enter in the installation instructions for your project"
+        },
+        {
+            type: "input",
+            name: "usageInformation",
+            message: "Please indicate how your project is intended to be used"
+        },
+        {
+            type: "input",
+            name: "contributionGuidelines",
+            message: "Please enter in the contribution guidelines"
+        },
+        {
+            type: "input",
+            name: "testInstructions",
+            message: "Please enter in the test instructions"
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Please indicate the license type for your project",
+            choices: ["Apache License 2.0", "GNU General Public License v3.0", "MIT License"]
+        },
+        {
+            type: "input",
+            name: "userRepo",
+            message: "Please enter in your github user name"
+        },
+        {
+            type: "input",
+            name: "userEmail",
+            message: "Please enter in your email address"
+        }
+    ])
+    .then((answers) => {
+        console.log(answers)
+        // Use user feedback for... whatever!!
+        writeAnswersToFile(answers);
+      })
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeAnswersToFile= (userAnswers)=>{
+    const initialData = 
+    `# ${userAnswers.projectTitle} 
+    \n## Description
+    \n ${userAnswers.description} 
+    \n## Installation 
+    \n ${userAnswers.installationInstructions} 
+    \n## Usage
+    \n ${userAnswers.usageInformation}
+    \n## Contributing
+    \n ${userAnswers.contributingGuidelines}
+    \n## Tests
+    \n ${userAnswers.testInstructions}
+    \n## License
+    \n ${userAnswers.license}
+    \n## Questions
+    \n Please find a link to my Github profile at github.com/${userAnswers.userRepo}/
+    \n For any questions, please email me at ${userAnswers.userEmail}`
+    
+    fs.writeFile(
+        `${userAnswers.projectTitle}.txt`, 
+        initialData, 
+    (error)=>error?console.log(error):console.log("success")
+    )
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
